6333df0906408879f197d52fb1b83e6dfb44071b
vision <visionthapa8540@gmail.com>
2026-06-19 12:34:25 +0545

    feat:post managemnt schemas and field

---
+++ schemas/post.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel,Field,HttpUrl
+from datetime import datetime
+from enum import Enum
+from typing import Literal,Optional
+
+class platform_opt(str,Enum):
+    instagram="instagram"
+    tiktok="tiktok"
+    facebook="facebook"
+
+class PostMasterBase(BaseModel):
+    content_url:HttpUrl=Field(description="claude storage link")
+    raw_description:str=Field(description="the descirption of value")
+
+class PostMasterCreate(PostMasterBase):
+    workspace_id:int
+
+class PostMasterResponse(PostMasterBase):
+    id:int
+    workspace_id:int
+    creator_id:int
+    ai_caption:Optional[str]=None
+    created_at:datetime    
+
+    model_config={"from_attributes":True}
+
+class PostChildBase(BaseModel):
+    platform:platform_opt
+
+class PostChildCreate(PostChildBase):
+    pass    
+
+class PostChildResponse(PostChildBase):
+    id:int
+    masterpost_id:int
+    approval_status:Literal["pending","approved","rejected"]="pending"
+    is_published:bool=False
+    published_at:Optional[datetime]=None
+
+    model_config={"from_attributes":True}
+
+
+
