20975a593f13d9a7b9e6c85947db1a057bb025d5
vision <visionthapa8540@gmail.com>
2026-06-24 08:18:09 +0545

    feat:made the user model with realtionship of wokspace

--- models/user.py
+++ models/user.py
@@ -1,21 +1,22 @@
-from sqlalchemy import Column,Text,String,Integer,Boolean,ForeignKey,DateTime
+from sqlalchemy import Column,Text,String,Integer,Boolean,ForeignKey,DateTime,Enum
 from sqlalchemy.orm import relationship,mapped_column,Mapped
 from sqlalchemy.sql import func
-from database import Base
+from database import Base,TimestampMixin
 from schemas import Role_type
-from enum import Enum
+from typing import TYPE_CHECKING
 
-class User(Base):
+if TYPE_CHECKING:
+    from models.workspace import Workspace
+
+class User(Base,TimestampMixin):
     __tablename__="users"
 
     id:Mapped[int]=mapped_column(Integer,primary_key=True,index=True)
     user_name:Mapped[str]=mapped_column(String(17),index=True,nullable=False,unique=True)
-    email:Mapped[str]=mapped_column(String(20),unique=True,nullable=False)
+    email:Mapped[str]=mapped_column(String(255),unique=True,nullable=False)
     role_type:Mapped[Role_type]=mapped_column(Enum(Role_type),nullable=False)
-    hashed_password:Mapped[str]=mapped_column(String(15),nullable=False)
-    created_at=Mapped[DateTime]=mapped_column(DateTime(timezone=True),server_default=func.now())
+    hashed_password:Mapped[str]=mapped_column(String(255),nullable=False)
 
-    
+    workspaces:Mapped[list["Workspace"]]=relationship(back_populates="owner")
 
 
-        
