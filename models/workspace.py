bdfdb3bf95e6ae583170b56742dace07662dfa9c
vision <visionthapa8540@gmail.com>
2026-06-24 08:12:41 +0545

    feat:made the model for databse

---
+++ models/workspace.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Integer,String,ForeignKey
+from database import Base,TimestampMixin
+from sqlalchemy.orm import relationship,Mapped,mapped_column
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from  models.user import User
+
+class Workspace(Base,TimestampMixin):
+    __tablename__="workspace"
+
+    id:Mapped[int]=mapped_column(Integer,primary_key=True,index=True)
+    brand_name:Mapped[str]=mapped_column(String(17),index=True,nullable=False)
+    owner_id:Mapped[int]=mapped_column(ForeignKey("users.id"),nullable=False)
+    
+    owner:Mapped["User"]=relationship(back_populates="workspaces")
+
+
