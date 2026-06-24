dabdcae65ef42fb554849387935e39b93fdba411
vision <visionthapa8540@gmail.com>
2026-06-24 18:57:22 +0545

    chore:basic  relation improvment

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,8 @@
 from database import Base
+from database  import TimestampMixin
 from models.user import User
 from models.workspace import Workspace
 
 
-__all__=["Base","User","Workspace"]
+__all__=["Base", "TimestampMixin", "User", "Workspace"]
 
