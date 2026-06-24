1f9cc9d83affa714bda52816dceeddd381b2a15a
vision <visionthapa8540@gmail.com>
2026-06-24 08:07:32 +0545

    chore:timestamp creation class

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm  import sessionmaker,DeclarativeBase
+from sqlalchemy import create_engine,Column,DateTime
+from sqlalchemy.orm  import sessionmaker,DeclarativeBase,Mapped,mapped_column
 from dotenv import load_dotenv
+from datetime import datetime
+from sqlalchemy.sql import func
+
 
 import os
 
@@ -20,6 +23,11 @@ SessionLocal=sessionmaker(
     bind=engine
 )
 
+class TimestampMixin:
+    created_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),server_default=func.now(),nullable=False)
+    updated_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),server_default=func.now(),onupdate=func.now,nullable=False)
+    
+
 class Base(DeclarativeBase):
     pass
 
@@ -28,4 +36,7 @@ def get_db():
     try:
         yield db
     finally:
-        db.close()    
+        db.close()  
+
+
+
