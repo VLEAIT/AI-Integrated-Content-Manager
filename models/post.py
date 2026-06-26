7f6bdde7b3e61d6f53a00f1c2d2576b234c1f68b
vision <visionthapa8540@gmail.com>
2026-06-26 17:31:25 +0545

    chor:change in class field

--- models/post.py
+++ models/post.py
@@ -1,8 +1,32 @@
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped,mapped_column
-from typing import TYPE_CHECKING
+from sqlalchemy import ForeignKey,String,Text,Enum,DateTime
+from sqlalchemy.orm import Mapped,mapped_column,relationship        
 from database import Base,TimestampMixin
+from schemas import platform_opt
+from schemas import status
 
 class PostMasterModel(Base,TimestampMixin):
+    __tablename__="post_master"
     id:Mapped[int]=mapped_column(primary_key=True)
     workspace_id:Mapped[int]=mapped_column(ForeignKey("workspace.id", ondelete="CASCADE"),nullable=True)
+    creator_id:Mapped[int]=mapped_column(ForeignKey("user.id", ondelete="CASCADE"),nullable=True)
+    content_url:Mapped[str]=mapped_column(Text,nullable=False)
+    raw_description:Mapped[str]=mapped_column(Text,nullable=False)
+    ai_caption:Mapped[str]=mapped_column(Text,nullable=False)
+
+    children:Mapped[list["PostChildModel"]]=relationship(back_populates="master", cascade="all, delete-orphan")
+
+
+
+class PostChildModel(Base,TimestampMixin):
+    __tablename__="post_child"
+    id:Mapped[int]=mapped_column(primary_key=True)
+    masterpost_id:Mapped[int]=mapped_column(ForeignKey("master.id",ondelete="CASCADE"),nullable=False)
+    platform:Mapped[platform_opt]=mapped_column(Enum(platform_opt),nullable=False)
+    approval_status:Mapped[status]=mapped_column(Enum(status),nullable=False)
+    is_published:Mapped[bool]=mapped_column(default=False, nullable=False)
+    boost_budget=Mapped[float]=mapped_column(default=0.0,nullable=False)
+    scheduled_time=Mapped[DateTime]=mapped_column(DateTime(timezone=True),default=None)
+    published_at=Mapped[DateTime]=mapped_column(DateTime(timezone=True),default=None)
+
+
+    master:Mapped["PostMasterModel"]=relationship(back_populates="children")
