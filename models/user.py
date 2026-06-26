0aa8831cd39a596f2ef6c49e0faf0a158a6f3128
vision <visionthapa8540@gmail.com>
2026-06-26 17:36:06 +0545

    fix:chnage in column name

--- models/user.py
+++ models/user.py
@@ -7,16 +7,18 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from models.workspace import Workspace
+    from models.workspacealloc import Workspacealloc
 
 class User(Base,TimestampMixin):
     __tablename__="users"
 
-    id:Mapped[int]=mapped_column(Integer,primary_key=True,index=True)
+    id:Mapped[int]=mapped_column(primary_key=True,index=True)
     user_name:Mapped[str]=mapped_column(String(17),index=True,nullable=False,unique=True)
     email:Mapped[str]=mapped_column(String(255),unique=True,nullable=False)
     role_type:Mapped[Role_type]=mapped_column(Enum(Role_type),nullable=False)
     hashed_password:Mapped[str]=mapped_column(String(255),nullable=False)
 
     workspaces:Mapped[list["Workspace"]]=relationship(back_populates="owner")
+    workspace_alloc:Mapped[list["Workspacealloc"]]=relationship(back_populates="user")
 
 
