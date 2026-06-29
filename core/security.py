2c5a4efa6809ee88bb1b15b3547b2ae1dfabb86f
vision <visionthapa8540@gmail.com>
2026-06-29 19:11:38 +0545

    feat: password hashing and oauth verification and token creation

--- core/security.py
+++ core/security.py
@@ -3,22 +3,24 @@ import os
 from datetime import timedelta,timezone,datetime
 from jose import jwt,JWTError
 from typing import Optional
-
-
-
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends,HTTPException,status
+from sqlalchemy.orm import Session
+from database import get_db
+from models.user import User
 
 
 
 
 pwd_context=CryptContext(
-    schemes="bcrypt",
+    schemes=["bcrypt"],
     deprecated="auto",
     bcrypt_handle_long_password=True
     )
 
 
 SECRET_KEY=os.getenv("SECRET_KEY")
-ALGORITHM="H256"
+ALGORITHM="HS256"
 TOKEN_EXPIRE_MINUTES=30
 
 def hashed_passowrd(password:str):
@@ -31,14 +33,40 @@ def create_token(data:dict)->str:
     to_encode=data.copy()
     expire=datetime.now(timezone.utc)+timedelta(minutes=TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp":expire})
-    return jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+    return jwt.encode(to_encode,SECRET_KEY,algorithm=[ALGORITHM])
 
 
 def decode(token:str)->Optional[dict]:
     try:
-        return jwt.decode(token,SECRET_KEY,algorithms=ALGORITHM)
+        return jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
     except JWTError:
         return None
 
 
+oauth2_schema=OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+def get_current_user(token:str=Depends(oauth2_schema),db:Session=Depends(get_db))->User:
+    credentail_exception=HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="crednetial validation error",
+        headers={"WWW-Authenticate":"Bearer"},
+        )
+    
+    try:
+        payload=decode(token)
+
+        if not payload:
+            raise credentail_exception
+        user_id:str=payload.get("sub")
+        if not user_id:
+            raise credentail_exception
+        
+    except (JWTError, ValueError):
+        raise credentail_exception
+
+    user = db.query(User).filter(User.id == int(user_id)).first()
+    if not user:
+       raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="user not found")
+      
+    return user  
 
