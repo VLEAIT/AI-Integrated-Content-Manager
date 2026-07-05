40f6eaae09b69e57622314e66a2adb46748b14a6
vision <visionthapa8540@gmail.com>
2026-07-05 14:58:21 +0545

    chore: added userdata update route

--- routers/auth.py
+++ routers/auth.py
@@ -1,17 +1,22 @@
 from fastapi import APIRouter,Depends,HTTPException,status
-from schemas import UserResponse,UserCreate,UserLogin,Token
+from schemas import UserResponse,UserCreate,UserLogin,Token,UserUpdate
 from database import get_db
 from sqlalchemy.orm import Session
 from models import User 
-from core import hashed_passowrd,verify_password,create_token
+from core import hashed_passowrd,verify_password,create_token,get_current_user
+from typing import Annotated
 
 router=APIRouter(
     prefix="/auth",
     tags=["auth"],
 )
 
+
+DatabaseSession =Annotated[Session,Depends(get_db)]
+CurrentUser=Annotated[User,Depends(get_current_user)]
+
 @router.post("/register", response_model=UserResponse,status_code=201)
-def register(user_register:UserCreate,db:Session=Depends(get_db)):
+def register(user_register:UserCreate,db:DatabaseSession):
     existing=db.query(User).filter(User.email==user_register.email).first()
     if existing:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
@@ -24,7 +29,7 @@ def register(user_register:UserCreate,db:Session=Depends(get_db)):
 
 
 @router.post("/login",response_model=Token)
-def login(user_login:UserLogin,db:Session=Depends(get_db)):
+def login(user_login:UserLogin,db:DatabaseSession):
     existing=db.query(User).filter(User.email==user_login.email).first()
     if not existing or  not verify_password(user_login.password,existing.hashed_password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="not found")
@@ -32,3 +37,25 @@ def login(user_login:UserLogin,db:Session=Depends(get_db)):
     return {"access_token":token,"token_type":"bearer"}
 
 
+@router.patch("/update",response_model=UserResponse)
+def update(user_update:UserUpdate,db:DatabaseSession,current_user:CurrentUser):
+    update_data=user_update.model_dump(exclude_unset=True)
+
+    if not update_data:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="field not filed")
+    
+    if "user_name" in update_data:
+        existing =db.query(User).filter(User.user_name==update_data["user_name"])
+        if existing and existing.id != current_user.id:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="User name already taken")
+        
+
+    for key,value in  update_data.items():
+        setattr(current_user,key,value)
+
+    db.add(current_user)
+    db.commit()
+    db.refresh(current_user)
+    return current_user
+
+        
