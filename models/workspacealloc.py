cb839b7b5c708dddf1e82d873fc91e4bd40d6420
vision <visionthapa8540@gmail.com>
2026-07-07 19:06:26 +0545

    chore: added allocated role column

--- models/workspacealloc.py
+++ models/workspacealloc.py
@@ -1,7 +1,8 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey,Enum
 from sqlalchemy.orm  import Mapped,mapped_column,relationship
 from database import Base,TimestampMixin
 from typing import TYPE_CHECKING
+from schemas import Role_type
 
 if TYPE_CHECKING:
     from models.user import User
@@ -16,6 +17,7 @@ class Workspacealloc(Base,TimestampMixin):
     user_id:Mapped[int]=mapped_column(ForeignKey("users.id",ondelete="CASCADE"),nullable=True)
     can_approve_posts:Mapped[bool]=mapped_column(default=False,nullable=False)
     can_access_ai:Mapped[bool]=mapped_column(default=False,nullable=False)
+    allocated_role:Mapped[Role_type]=mapped_column(Enum(Role_type),nullable=False)
 
     user :Mapped["User"]=relationship(back_populates="workspace_alloc")
     workspaceall:Mapped["Workspace"]=relationship(back_populates="member_alloc")
