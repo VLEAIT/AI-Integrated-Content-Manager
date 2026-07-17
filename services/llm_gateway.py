d3dc3aa71c2c9dbc31a45fd8e656ba08bf5083cf
vision <visionthapa8540@gmail.com>
2026-07-17 09:31:01 +0545

    chore: update in llm gateway

--- services/llm_gateway.py
+++ services/llm_gateway.py
@@ -14,7 +14,30 @@ class LLMGateway:
             self.client=None
 
     def generate_social_caption(self, raw_description:str)->str:
-                
-
-
+        if not self.client:
+            return "Caption generation is offline (Missing api key)"
+        if  self.provider == "claude":
+            return self._call_claude(raw_description)
+        
+        def _call_claude(self,prompt:str)->str:
+            system_instruction=(
+                "You are an expert social media copywriter and growth marketer."
+                " Your task is to generate high-converting, platform-optimized captions for marketing posts and short-form video reels based on the user's provided context, images, or raw transcripts."
+                "Output only the final caption without quotas")
+            try:
+                response=self.client.messages.create(
+                    model="claude-3-5-sonnet-20241022",
+                    max_token=1000,
+                    temperature=0.7,
+                    system=system_instruction,
+                    message=[
+                        {"role":"user","content":f"Create a master caption for this:\n{prompt}"}
+                    ]        
+                )
+                return response.content[0].text.strip()
+            except Exception as e:
+                print(f"LLM Provider Error:{str(e)}")
+                return "Caption generation failed.please try again"
+            
+ai_gateway=LLMGateway()           
 
