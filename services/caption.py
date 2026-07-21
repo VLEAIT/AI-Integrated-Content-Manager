14b4d746da4984695379c3eee5a262ae7b0b866e
vision <visionthapa8540@gmail.com>
2026-07-21 16:01:49 +0545

    chore: update in caption

--- services/caption.py
+++ services/caption.py
@@ -1,31 +1,39 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session,Sess
 from  models import PostMasterModel,AIstatus
 from llm_gateway import ai_gateway
-from database import get_db
+from database import get_db,SessionLocal
 from typing import Annotated
 from fastapi import Depends
 
-databasesession=Annotated[Session,Depends(get_db)]
 
 
-def process_master_post_caption(masterpost_id:int,db:databasesession):
-    post=db.query(PostMasterModel).filter(PostMasterModel.id==masterpost_id).first()
-    if not post:
-        return
-    post.caption_ai_status=AIstatus.PROCESSING
-    db.commit()
-
+def process_master_post_caption(masterpost_id:int)->None:
+    db=SessionLocal()
+    post=None
     try:
-        generated_caption=ai_gateway.generate_social_caption(post.raw_description)
+        post=db.query(PostMasterModel).filter(PostMasterModel.id==masterpost_id).first()
+        if not post:
+            return
+        post.caption_ai_status=AIstatus.PROCESSING
+        db.commit()
 
-        if generated_caption.startswith("["):
-            raise Exception(f"AI generated issue:{generated_caption}")
-        
-        post.ai_caption=generated_caption
+        ai_text=ai_gateway.generate_social_caption(prompt=post.raw_description)
+        post.ai_caption=ai_text
         post.caption_ai_status=AIstatus.COMPLETED
+        post.caption_ai_error=None
+        db.commit()
 
     except Exception as e:
-        post.caption_ai_status=AIstatus.FAILED
-        post.caption_ai_error=str(e)
+        db.rollback()
+
+        if post:
+            post.caption_ai_status=AIstatus.FAILED
+            post.caption_ai_error=str(e)
+            db.commit()
+
     finally:
-        db.commit()        
+        db.close()        
+
+
+
+ 
