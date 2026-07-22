b1a399270d8866a8a614ff99559b33732f7813e9
vision <visionthapa8540@gmail.com>
2026-07-22 14:19:57 +0545

    fix: fixed the bug

--- database.py
+++ database.py
@@ -25,7 +25,7 @@ SessionLocal=sessionmaker(
 
 class TimestampMixin:
     created_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),server_default=func.now(),nullable=False)
-    updated_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),server_default=func.now(),onupdate=func.now,nullable=False)
+    updated_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),server_default=func.now(),onupdate=func.now(),nullable=False)
     
 
 class Base(DeclarativeBase):
