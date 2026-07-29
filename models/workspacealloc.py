91abeca8d1f0b7d6a7107657df684a562c6afbbd
vision <visionthapa8540@gmail.com>
2026-07-30 04:41:20 +0545

    fix: init addition in workspacealloc

--- models/workspacealloc.py
+++ models/workspacealloc.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey,Enum
+from sqlalchemy import ForeignKey,Enum as SQLEnum
 from sqlalchemy.orm  import Mapped,mapped_column,relationship
 from database import Base,TimestampMixin
 from typing import TYPE_CHECKING
@@ -12,13 +12,13 @@ if TYPE_CHECKING:
 class Workspacealloc(Base,TimestampMixin):
     __tablename__="workspacealloc"
 
-    id:Mapped[int]=mapped_column(primary_key=True)
+    id:Mapped[int]=mapped_column(primary_key=True,init=False)
     workspace_id:Mapped[int]=mapped_column(ForeignKey("workspace.id", ondelete="CASCADE"),nullable=True)
     user_id:Mapped[int]=mapped_column(ForeignKey("users.id",ondelete="CASCADE"),nullable=True)
-    can_approve_posts:Mapped[bool]=mapped_column(default=False,nullable=False)
-    can_access_ai:Mapped[bool]=mapped_column(default=False,nullable=False)
-    allocated_role:Mapped[Role_type]=mapped_column(Enum(Role_type),nullable=False)
+    allocated_role:Mapped[Role_type]=mapped_column(SQLEnum(Role_type),nullable=False,init=False)
+    can_approve_posts:Mapped[bool]=mapped_column(default=False,init=False)
+    can_access_ai:Mapped[bool]=mapped_column(default=False,init=False)
 
-    user :Mapped["User"]=relationship(back_populates="workspace_alloc")
-    workspaceall:Mapped["Workspace"]=relationship(back_populates="member_alloc")
+    user :Mapped["User"]=relationship(back_populates="workspace_alloc",init=False)
+    workspaceall:Mapped["Workspace"]=relationship(back_populates="member_alloc",init=False)
 
