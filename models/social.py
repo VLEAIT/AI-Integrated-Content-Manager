58ba29a312936c8a018686a1e359ee5cefc6cd41
vision <visionthapa8540@gmail.com>
2026-07-29 06:27:23 +0545

    chore: added platform accout id

--- models/social.py
+++ models/social.py
@@ -1,17 +1,16 @@
-from sqlalchemy import column,Integer,String,Text,ForeignKey,Enum as SQLEnum,Boolean,DateTime
+from sqlalchemy import Integer,String,Text,ForeignKey,Enum as SQLEnum,Boolean
 from sqlalchemy.orm import Mapped,mapped_column
-from database import Base
+from database import Base,TimestampMixin
 from schemas import platform_opt
-from datetime import datetime
-from sqlalchemy.sql import func
 
-class SocialAccount(Base):
+class SocialAccount(Base,TimestampMixin):
     __tablename__="social_accounts"
 
     id:Mapped[int]=mapped_column(Integer,nullable=False,primary_key=True,index=True)
     workspace_id:Mapped[int]=mapped_column(Integer,ForeignKey("workspace.id",ondelete="CASCADE"),nullable=False)
     platform:Mapped[platform_opt]=mapped_column(SQLEnum(platform_opt,native_enum=False),nullable=False)
     account_name:Mapped[str]=mapped_column(String,nullable=False)
+    platform_account_id:Mapped[str]=mapped_column(String,nullable=False)
     access_token:Mapped[str]=mapped_column(String,nullable=False)
     is_active:Mapped[bool]=mapped_column(Boolean,default=True,nullable=False)
-    created_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),server_default=func.now(),nullable=False)
+   
