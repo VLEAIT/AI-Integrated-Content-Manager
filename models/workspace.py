e2df21be545e995aaed64e72c8a59e900d3bc80d
vision <visionthapa8540@gmail.com>
2026-07-30 05:05:57 +0545

    chore: added init value

--- models/workspace.py
+++ models/workspace.py
@@ -10,10 +10,10 @@ if TYPE_CHECKING:
 class Workspace(Base,TimestampMixin):
     __tablename__="workspace"
 
-    id:Mapped[int]=mapped_column(Integer,primary_key=True,index=True)
+    id:Mapped[int]=mapped_column(Integer,primary_key=True,init=False)
     brand_name:Mapped[str]=mapped_column(String(17),index=True,nullable=False)
     owner_id:Mapped[int]=mapped_column(ForeignKey("users.id"),nullable=False)
     
-    owner:Mapped["User"]=relationship(back_populates="workspaces")
-    member_alloc:Mapped[list["Workspacealloc"]]=relationship(back_populates="workspaceall")
+    owner:Mapped["User"]=relationship(back_populates="workspaces",init=False)
+    member_alloc:Mapped[list["Workspacealloc"]]=relationship(back_populates="workspaceall",init=False)
 
