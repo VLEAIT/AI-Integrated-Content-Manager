c6d0d8b78654dece84e68a3497dabc594b77dbd3
vision <visionthapa8540@gmail.com>
2026-07-30 04:36:12 +0545

    fix: made changes in field placing and init false in relationship

--- models/post.py
+++ models/post.py
@@ -18,17 +18,18 @@ class AIstatus(str,Enum):
 
 class PostMasterModel(Base,TimestampMixin):
     __tablename__="post_master"
-    id:Mapped[int]=mapped_column(primary_key=True)
-    workspace_id:Mapped[int]=mapped_column(ForeignKey("workspace.id", ondelete="CASCADE"),nullable=True)
-    creator_id:Mapped[int]=mapped_column(ForeignKey("users.id", ondelete="CASCADE"),nullable=True)
+    id:Mapped[int]=mapped_column(primary_key=True,init=False)
     content_url:Mapped[str]=mapped_column(Text,nullable=False)
     raw_description:Mapped[str]=mapped_column(Text,nullable=False)
-    ai_caption:Mapped[Optional[str]]=mapped_column(Text,nullable=True)
+    workspace_id:Mapped[int]=mapped_column(ForeignKey("workspace.id", ondelete="CASCADE"),nullable=True,default=None)
+    creator_id:Mapped[int]=mapped_column(ForeignKey("users.id", ondelete="CASCADE"),nullable=True,default=None)
+
+    ai_caption:Mapped[Optional[str]]=mapped_column(Text,nullable=True,default=None)
     caption_ai_status:Mapped[AIstatus]=mapped_column(SQLEnum(AIstatus),default=AIstatus.QUEUED,nullable=False)
-    caption_ai_error:Mapped[Optional[str]]=mapped_column(Text,nullable=True)
+    caption_ai_error:Mapped[Optional[str]]=mapped_column(Text,nullable=True,default=None)
  
 
-    children:Mapped[List["PostChildModel"]]=relationship(back_populates="master", cascade="all, delete-orphan")
+    children:Mapped[List["PostChildModel"]]=relationship(back_populates="master", cascade="all, delete-orphan",init=False)
 
 
 
@@ -36,13 +37,13 @@ class PostChildModel(Base,TimestampMixin):
     __tablename__="post_child"
     id:Mapped[int]=mapped_column(primary_key=True,init=False)
     masterpost_id:Mapped[int]=mapped_column(ForeignKey("post_master.id",ondelete="CASCADE"),nullable=False)
+    social_account_id:Mapped[int]=mapped_column(ForeignKey("social_accounts.id",ondelete="CASCADE"),nullable=False)
     platform:Mapped[platform_opt]=mapped_column(SQLEnum(platform_opt),nullable=False,init=False)
     approval_status:Mapped[statu]=mapped_column(SQLEnum(statu),default=statu.pending,init=False)
     is_published:Mapped[bool]=mapped_column(default=False, init=False)
     boost_budget:Mapped[float]=mapped_column(default=0.0,init=False)
-    social_account_id:Mapped[int]=mapped_column(ForeignKey("social_accounts.id",ondelete="CASCADE"),nullable=False)
-    scheduled_time:Mapped[datetime]=mapped_column(DateTime(timezone=True),default=None)
-    published_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),default=None)
+    scheduled_time:Mapped[datetime]=mapped_column(DateTime(timezone=True),nullable=True,default=None)
+    published_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),nullable=True,default=None)
 
 
-    master:Mapped["PostMasterModel"]=relationship(back_populates="children")
+    master:Mapped["PostMasterModel"]=relationship(back_populates="children",init=False)
